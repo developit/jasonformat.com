fe9cf4f71dcd83af220bc2b108ebfa6975a64b9f
Jason Miller <developit@users.noreply.github.com>
2021-04-14 12:37:17 -0400

    Optimize hero image

--- public/components/Home/index.js
+++ public/components/Home/index.js
@@ -3,10 +3,13 @@ import config from '../../config.json';
 import styles from './style.module.scss';
 
 export default function Home() {
+	if (typeof window === 'undefined') {
+		document.head.insertAdjacentHTML('beforeend', `<link rel="preload" as="image" href="${config.cover}">`);
+	}
 	return (
 		<section class="home">
 			<header class={styles.jumbotron}>
-				{config.cover && <div class={styles.cover} style={{ backgroundImage: `url(${config.cover})` }} />}
+				{config.cover && <div class={styles.cover} style={{ backgroundImage: `url('${config.cover}')` }} />}
 				<h1>{config.title}</h1>
 				<p>{config.description}</p>
 			</header>
