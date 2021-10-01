d82684f724aefcba9f8da37182414e157d2963d1
Emmanuel DEMEY <demey.emmanuel@gmail.com>
2021-10-01 21:27:34 +0200

    feat: definie dedicated title for all pages (#7)

--- public/components/NotFound/index.js
+++ public/components/NotFound/index.js
@@ -1,8 +1,14 @@
-const NotFound = () => (
-	<section class="content">
-		<h1>404: Not Found</h1>
-		<p>It's gone :(</p>
-	</section>
-);
+import config from '../../config.json';
+
+const NotFound = () => {
+	document.title = config.title;
+
+	return (
+		<section className="content">
+			<h1>404: Not Found</h1>
+			<p>It's gone :(</p>
+		</section>
+	);
+}
 
 export default NotFound;
